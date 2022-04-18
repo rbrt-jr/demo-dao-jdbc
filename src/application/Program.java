7219b3ecca0f72c8f6605315263f89a89d241f1d
Roberto Silva <roberto_silva_jr@outlook.pt>
2022-04-18 19:56:41 +0100

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(2, "Alan de Lon", "alan@sapo.pt", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 	}
 
 }
